e7979baf36be680ff941de70ee4b293b57207a5c
raccoonrocket <9802432172sergei@gmail.com>
2024-11-27 22:58:01 +0000

    fix for testing

--- update_and_move.sh
+++ update_and_move.sh
@@ -5,16 +5,14 @@ set -e
 
 # Инициализация и обновление субмодулей
 echo "Инициализация и обновление субмодулей..."
-cd search_engine_crawler/ && git submodule init
-cd ..
-cd search_engine_ui/ && git submodule update
-cd ..
+cd search_engine_crawler/ && git submodule init && git submodule update && cd ..
+cd search_engine_ui/ && git submodule init && git submodule update && cd ..
 
 # Перемещение файлов
 echo "Перемещение файлов..."
-mv submodule_updates/search_engine_crawler/Dockerfile search_engine_crawler/Dockerfile
-mv submodule_updates/search_engine_ui/Dockerfile search_engine_ui/Dockerfile
-mv submodule_updates/search_engine_ui/index.html search_engine_ui/ui/templates/index.html
+cp submodule_updates/search_engine_crawler/Dockerfile search_engine_crawler/Dockerfile
+cp submodule_updates/search_engine_ui/Dockerfile search_engine_ui/Dockerfile
+cp submodule_updates/search_engine_ui/index.html search_engine_ui/ui/templates/index.html
 
 # Проверяем успешность перемещения
 if [ $? -eq 0 ]; then
